7aa8f404a87c185491dec4c06a7a103f21123243
knyghtmare <tahsinjahinkhalid@gmail.com>
2020-01-29 17:05:32 +0600

    added venv to project

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ screen = pygame.display.set_mode((width,height))
 
 # add title and icon
 pygame.display.set_caption("Tahsin Jahin's 2D shooter Game")
-icon = pygame.image.load('game-icon.png')
+icon = pygame.image.load('images/game-icon.png')
 pygame.display.set_icon(icon)
 
 # background
-background = pygame.image.load('background.jpg')
+background = pygame.image.load('images/background.jpg')
 
 # define the player
 class Player:
@@ -111,14 +111,14 @@ def update_display(window, player):
     pygame.display.update()
 
 
-player_img = pygame.image.load('player.png')
+player_img = pygame.image.load('images/player/player.png')
 you_x = 60
 you_y = random.randint(60, 640)
-monster_img1 = pygame.image.load('monster1.png')
-monster_img2 = pygame.image.load('monster2.png')
-monster_img3 = pygame.image.load('monster3.png')
-monster_img4 = pygame.image.load('monster4.png')
-monster_img5 = pygame.image.load('monster5.png')
+monster_img1 = pygame.image.load('images/monsters/monster1.png')
+monster_img2 = pygame.image.load('images/monsters/monster2.png')
+monster_img3 = pygame.image.load('images/monsters/monster3.png')
+monster_img4 = pygame.image.load('images/monsters/monster4.png')
+monster_img5 = pygame.image.load('images/monsters/monster5.png')
 mon_x = 1010
 mon_y1 = random.randint(60, 640)
 mon_y2 = random.randint(60, 640)
